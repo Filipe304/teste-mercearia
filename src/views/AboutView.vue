4806724596d96197c1895e3ec8bb98b5dc96777f
filipe304 <filipe.diablovt@hotmail.com>
2022-11-01 11:21:34 -0300

    inserindo chart.js

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -97,24 +97,17 @@
             <v-card elevation="10" class="header-principal">
             Delícias da Vovó
             </v-card>
-          
-
-          
-
           </v-col>
         </v-row>
         <v-row cols="12">
           <v-col md="12">
-
             <v-card elevation="10" class="conteudo-geral">
               <v-title-card style="color: #000000;">
                 aqui title  
             </v-title-card>
-
             <div>
-              aqui ...
+              <canvas id="myChart" width="400" height="400"></canvas>
             </div>
-
             </v-card> 
           </v-col>
         </v-row>
@@ -124,7 +117,7 @@
   </div>
   </v-container>
 </template>
-
+<script src="path/to/chartjs/dist/chart.js"></script>
 <script>
 export default {
   methods: {
@@ -151,17 +144,49 @@ export default {
           { title: 'Produtos',
           icon: 'mdi-cake',
           link: ''},
-<<<<<<< HEAD
-
-          { title: 'Sair',
-          icon: 'mdi-arrow-left-bold-outline',
-          link: 'Home'}
-=======
->>>>>>> 905ee75 (reparo btn de sair)
         ],
         mini: true,
       }
     },
+    mounted () {
+      const ctx = document.getElementById('myChart');
+      const Chart = require('chart.js');
+      const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+    myChart;
+    }
 };
 </script>
 
